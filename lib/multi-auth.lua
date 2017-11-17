737d84485f3d7724366e36ae00b719c1f5ddb229
Alex Ginzburg <alex.ginzburg@refinery29.com>
2017-11-17 12:21:18 -0500

    a better way to get an auth token from either header or a query string parameter
    Signed-off-by: Alex Ginzburg <alex.ginzburg@refinery29.com>

--- lib/multi-auth.lua
+++ lib/multi-auth.lua
@@ -8,14 +8,13 @@ local whitelist_ips = multiauth_ip_whitelist or {}
 
 whitelist = iputils.parse_cidrs(whitelist_ips)
 
-local header_token_auth = ngx.var.http_x_token_auth
-local query_string_token_auth = ngx.var.arg_x_token_auth
+local token_auth = ngx.var.http_x_token_auth and ngx.var_http_x_token_auth or ngx.var.arg_x_token_auth
 
 if iputils.ip_in_cidrs(ngx.var.remote_addr, whitelist) then
   ngx.log(ngx.ERR, "Requestor " .. ngx.var.remote_addr .. " on whitelist")
-elseif header_token_auth or query_string_token_auth then
+elseif token_auth then
   ngx.log(ngx.ERR, "Checking auth header...")
-  token_access.check_token(token_auth, query_string_token_auth)
+  token_access.check_token(token_auth)
 else
   ngx.log(ngx.ERR, "Validating oauth...")
   oauth_access.validate()
